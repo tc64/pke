088ebf1291e1d4eb8a96825820eb11f39cec8bf5
taylor cassidy <taylorcassidy64@gmail.com>
2020-03-29 19:43:40 -0400

    allow metadata to force inclusion in textrank graph

--- pke/unsupervised/graph_based/textrank.py
+++ pke/unsupervised/graph_based/textrank.py
@@ -109,10 +109,10 @@ class TextRank(LoadFile):
             pos = {'NOUN', 'PROPN', 'ADJ'}
 
         def isvalid(sentence, i):
-            meta_force = sentence.meta.get('force', [])
+            meta_force = sentence.meta.get('force', set())
             force = False
             if meta_force and len(meta_force) > i:
-                force = meta_force[i]
+                force = i in meta_force
             return (sentence.pos[i] in pos) or force
 
         # flatten document as a sequence of (word, pass_syntactic_filter) tuples
